aedb60caeb2728279ff75f229cbefdd9c3b1ffec
pp221B031123 <98882902+pp221B031123@users.noreply.github.com>
2023-02-22 18:45:28 +0600

    Add files via upload

---
+++ Lab4/my-app/src/app/products.ts
@@ -0,0 +1,104 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  rating: string;
+  image_url: string;
+  kaspi_link: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Apple iPhone 13 128Gb черный',
+    price: 373590,
+    description: 'технология NFC: Да цвет: черный тип экрана: OLED, Super Retina XDR диагональ: 6.1 дюйм размер оперативной памяти: 4 ГБ процессор: 6-ядерный Apple A15 Bionic объем встроенной памяти: 128 ГБ емкость аккумулятора: 3095 мАч',
+    rating: '5/5',
+    image_url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h49/hc0/46392662523934/apple-iphone-13-128gb-cernyj-102298404-1-Container.jpg',
+    kaspi_link: 'https://kaspi.kz/shop/p/apple-iphone-13-128gb-chernyi-102298404/?c=750000000#!/item'
+  },
+  {
+    id: 2,
+    name: 'Apple iPhone 14 128Gb черный',
+    price: 420608,
+    description: 'технология NFC: Да цвет: черный тип экрана: OLED, Super Retina XDR display диагональ: 6.1 дюйм размер оперативной памяти: 6 ГБ процессор: 6-ядерный Apple A15 Bionic объем встроенной памяти: 128 ГБ емкость аккумулятора: 3279 мАч',
+    rating: '5/5',
+    image_url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h8f/h61/63072540131358/apple-iphone-14-128gb-cernyj-106363023-1.jpg',
+    kaspi_link: 'https://kaspi.kz/shop/p/apple-iphone-14-128gb-chernyi-106363023/?c=750000000#!/item'
+  },
+  {
+    id: 3,
+    name: 'Apple iPhone 13 128Gb синий',
+    price: 378940,
+    description: 'технология NFC: Да цвет: синий тип экрана: OLED, Super Retina XDR диагональ: 6.1 дюйм размер оперативной памяти: 4 ГБ процессор: 6-ядерный Apple A15 Bionic объем встроенной памяти: 128 ГБ емкость аккумулятора: 3095 мАч',
+    rating: '5/5',
+    image_url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h91/h1d/46392661737502/apple-iphone-13-128gb-sinij-102298364-1-Container.jpg',
+    kaspi_link: 'https://kaspi.kz/shop/p/apple-iphone-13-128gb-sinii-102298364/?c=750000000#!/item'
+  },
+  {
+    id: 4,
+    name: 'Apple iPhone 13 128Gb белый',
+    price: 372708,
+    description: 'технология NFC: Да цвет: белый тип экрана: OLED, Super Retina XDR диагональ: 6.1 дюйм размер оперативной памяти: 4 ГБ процессор: 6-ядерный Apple A15 Bionic объем встроенной памяти: 128 ГБ емкость аккумулятора: 3095 мАч',
+    rating: '5/5',
+    image_url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h96/h3c/46392664227870/apple-iphone-13-128gb-belyj-102298420-1-Container.jpg',
+    kaspi_link: 'https://kaspi.kz/shop/p/apple-iphone-13-128gb-belyi-102298420/?c=750000000#!/item'
+  },
+  {
+    id: 5,
+    name: 'Apple iPhone 14 Pro 256Gb фиолетовый',
+    price: 628816,
+    description: 'технология NFC: Да цвет: фиолетовый тип экрана: OLED, Super Retina XDR display с возможностью постоянной работы диагональ: 6.1 дюйм размер оперативной памяти: 6 ГБ процессор: 6-ядерный Apple A16 Bionic объем встроенной памяти: 256 ГБ емкость аккумулятора: 3125 мАч',
+    rating: '5/5',
+    image_url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h4d/h62/62948396728350/apple-iphone-14-pro-128gb-fioletovyj-106363319-1.jpg',
+    kaspi_link: 'https://kaspi.kz/shop/p/apple-iphone-14-pro-256gb-fioletovyi-106363319/?c=750000000#!/item'
+  },
+  {
+    id: 6,
+    name: 'Apple iPhone 14 Pro 128Gb фиолетовый',
+    price: 583069,
+    description: 'технология NFC: Да цвет: фиолетовый тип экрана: OLED, Super Retina XDR display с возможностью постоянной работы диагональ: 6.1 дюйм размер оперативной памяти: 6 ГБ процессор: 6-ядерный Apple A16 Bionic объем встроенной памяти: 128 ГБ емкость аккумулятора: 3125 мАч',
+    rating: '5/5',
+    image_url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hfe/hf7/62948389748766/apple-iphone-14-pro-128gb-fioletovyj-106363283-1.jpg',
+    kaspi_link: 'https://kaspi.kz/shop/p/apple-iphone-14-pro-128gb-fioletovyi-106363283/?c=750000000#!/item'
+  },
+  {
+    id: 7,
+    name: 'Apple iPhone 14 128Gb голубой',
+    price: 421180,
+    description: 'технология NFC: Да цвет: голубой тип экрана: OLED, Super Retina XDR display диагональ: 6.1 дюйм размер оперативной памяти: 6 ГБ процессор: 6-ядерный Apple A15 Bionic объем встроенной памяти: 128 ГБ емкость аккумулятора: 3279 мАч',
+    rating: '5/5',
+    image_url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h8b/h41/63073057177630/apple-iphone-14-128gb-goluboj-106363150-1.jpg',
+    kaspi_link: 'https://kaspi.kz/shop/p/apple-iphone-14-128gb-goluboi-106363150/?c=750000000#!/item'
+  },
+  {
+    id: 8,
+    name: 'Apple iPhone 11 128Gb Slim Box белый',
+    price: 296700,
+    description: 'технология NFC: Да цвет: белый тип экрана: сенсорный мультитач цветной IPS, Liquid Retina HD диагональ: 6.1 дюйм размер оперативной памяти: 4 ГБ процессор: 6-ядерный Apple A13 Bionic объем встроенной памяти: 128 ГБ емкость аккумулятора: 3110 мАч',
+    rating: '5/5',
+    image_url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h3a/hb4/33208264228894/apple-iphone-11-128gb-slim-box-belyj-100692385-1-Container.jpg',
+    kaspi_link: 'https://kaspi.kz/shop/p/apple-iphone-11-128gb-slim-box-belyi-100692385/?c=750000000#!/item'
+  },
+  {
+    id: 9,
+    name: 'Apple iPhone 13 128Gb розовый',
+    price: 374415 ,
+    description: 'технология NFC: Да цвет: розовый тип экрана: OLED, Super Retina XDR диагональ: 6.1 дюйм размер оперативной памяти: 4 ГБ процессор: 6-ядерный Apple A15 Bionic объем встроенной памяти: 128 ГБ емкость аккумулятора: 3095 мАч',
+    rating: '5/5',
+    image_url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/ha0/h0e/46392660819998/apple-iphone-13-128gb-rozovyj-102298145-1-Container.jpg',
+    kaspi_link: 'https://kaspi.kz/shop/p/apple-iphone-13-128gb-rozovyi-102298145/?c=750000000#!/item'
+  },
+  {
+    id: 10,
+    name: 'Apple iPhone 11 128Gb Slim Box черный',
+    price: 292769,
+    description: 'ттехнология NFC: Да цвет: черный тип экрана: сенсорный мультитач цветной IPS, Liquid Retina HD диагональ: 6.1 дюйм размер оперативной памяти: 4 ГБ процессор: 6-ядерный Apple A13 Bionic объем встроенной памяти: 128 ГБ емкость аккумулятора: 3110 мАч',
+    rating: '5/5',
+    image_url: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h17/h14/31489167425566/apple-iphone-11-128gb-slim-box-cernyj-100692388-1-Container.jpg',
+    kaspi_link: 'https://kaspi.kz/shop/p/apple-iphone-11-128gb-slim-box-chernyi-100692388/?c=750000000#!/item'
+  },
+
+
+];
